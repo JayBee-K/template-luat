03bfed0458602a4f77c8ce528d83b912171cb082
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-10-25 13:35:55 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,63 @@
 	'use strict';
 	let windowWidth = $(window).width();
 
+	let initSliderBanner = function () {
+		if ($('#slider-banner').length > 0) {
+			new Swiper('#slider-banner .swiper', {
+				loop: false,
+				effect: "fade",
+				speed: 450,
+				pagination: {
+					el: '#slider-banner .slider-theme_actions--pagination',
+					clickable: true,
+				},
+				autoplay: {
+					delay: 10000,
+					disableOnInteraction: false,
+				},
+			});
+		}
+	}
+	let initSliderArticle = function () {
+		if ($('#slider-articles').length > 0) {
+			new Swiper('#slider-articles .swiper', {
+				loop: false,
+				speed: 450,
+				spaceBetween: 30,
+				autoplay: {
+					delay: 10000,
+					disableOnInteraction: false,
+				},
+				navigation: {
+					nextEl: '#slider-articles .slider-theme_actions--next',
+					prevEl: '#slider-articles .slider-theme_actions--prev',
+				},
+				breakpoints: {
+					1200: {
+						slidesPerView: 4,
+					},
+					991: {
+						slidesPerView: 3.3,
+					},
+					768: {
+						slidesPerView: 2.3,
+					},
+					525: {
+						slidesPerView: 1.5,
+					},
+					375: {
+						slidesPerView: 1.3,
+					},
+					320: {
+						slidesPerView: 1,
+					},
+				},
+			});
+		}
+	}
+
 	$(function () {
+		initSliderBanner();
+		initSliderArticle();
 	});
 })(jQuery);
